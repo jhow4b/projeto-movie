44228d36226b8b00133e41ef320f41291299cd6b
jhow4b <103279592+jhow4b@users.noreply.github.com>
2024-08-09 23:03:16 -0300

    Projeto Concluído

--- script.js
+++ script.js
@@ -5,12 +5,22 @@ arrows.forEach((arrow, i) => {
     const itemNumber = movieLists[i].querySelectorAll("img").length
     let clickCounter = 0
     arrow.addEventListener("click", () => {
+        const ratio = Math.floor(window.innerWidth / 270)
         clickCounter++
-        if ((itemNumber - 2) - (4 + clickCounter) >= 0) {
+        if ((itemNumber) - (4 + clickCounter) + (4 - ratio) >= 0) {
             movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 300}px)`
         } else {
             movieLists[i].style.transform = "translateX(0)"
             clickCounter = 0
         }
+    })
+})
+
+const toggle = document.querySelector(".toggle")
+const itens = document.querySelectorAll(".container, .movie-list-title, .navbar-container, .sidebar, .left-menu-icon, .toggle, .toggle-ball")
+
+toggle.addEventListener("click", () => {
+    itens.forEach(item => {
+        item.classList.toggle("active")
     })
 })
